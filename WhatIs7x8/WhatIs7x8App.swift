e35744b9419311ae8b64b45612c1ce2c3717d0f4
Esben Viskum <viskum@viskum.dk>
2021-04-09 10:43:59 +0200

    Initial Commit

---
+++ WhatIs7x8/WhatIs7x8App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIs7x8App.swift
+//  WhatIs7x8
+//
+//  Created by Esben Viskum on 09/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct WhatIs7x8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
