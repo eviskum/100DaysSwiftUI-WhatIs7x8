797bbf62f36820b758b151e8525ae14d6dec5d72
Esben Viskum <viskum@viskum.dk>
2021-04-12 11:49:43 +0200

    Mul operator is working now

--- WhatIs7x8/ContentView.swift
+++ WhatIs7x8/ContentView.swift
@@ -7,6 +7,83 @@
 
 import SwiftUI
 
+enum Level {
+    case easy
+    case medium
+    case hard
+    case expert
+}
+
+enum Operation {
+    case mul
+    case div
+    case add
+    case sub
+    
+    func maxNumber(level: Level) -> Int {
+        switch (self) {
+        case .mul:
+            switch (level) {
+            case .easy:
+                return 5
+            case .medium:
+                return 10
+            case .hard:
+                return 20
+            case .expert:
+                return 50
+            }
+        case .div:
+            switch (level) {
+            case .easy:
+                return 5
+            case .medium:
+                return 10
+            case .hard:
+                return 20
+            case .expert:
+                return 50
+            }
+        case .add:
+            switch (level) {
+            case .easy:
+                return 5
+            case .medium:
+                return 20
+            case .hard:
+                return 50
+            case .expert:
+                return 100
+            }
+        case .sub:
+            switch (level) {
+            case .easy:
+                return 5
+            case .medium:
+                return 20
+            case .hard:
+                return 50
+            case .expert:
+                return 100
+            }
+        }
+    }
+    
+    func maxAnswer(number: Int, level: Level) -> Int {
+        switch (self) {
+        case .mul:
+            return maxNumber(level: level) * number * 2
+        case .div:
+            return maxNumber(level: level) * 2
+        case .add:
+            return (maxNumber(level: level) + number) * 2
+        case .sub:
+            return maxNumber(level: level) + number
+        }
+    }
+}
+
+
 struct Operators {
     let mul: Bool
     let div: Bool
@@ -41,6 +118,7 @@ struct Operators {
     mutating func calcNoOfQuestionsPerOperator(noOfQs: Int) {
         let noOfOps = noOfOperators()
         let noOfQuestionsPerOperator: Int = Int(noOfQs / noOfOps)
+        
         if mul { mulQuestions = noOfQuestionsPerOperator }
         if div { divQuestions = noOfQuestionsPerOperator }
         if add { addQuestions = noOfQuestionsPerOperator }
@@ -51,7 +129,7 @@ struct Operators {
         var tempAdd: Bool = add
         var tempSub: Bool = sub
         
-        var remainder = Int(noOfQs / noOfOps)
+        var remainder = Int(noOfQs % noOfOps)
         while remainder > 0 {
             let randomOp = Int.random(in: 0...3)
             switch randomOp {
@@ -86,43 +164,95 @@ struct Operators {
 
 struct Question {
     let question: String
-    let answer: Int
+    var answers: [Int] = []
+    var correctAnswerIdx: Int = 0
     
-    init(factor1: Int, factor2: Int) {
-        question = String("\(factor1) x \(factor2)")
-        answer = factor1 * factor2
+    init(operation: Operation, level: Level, no1: Int, no2: Int) {
+        var answer: Int
+        switch operation {
+        case .mul:
+            question = String("\(no1) x \(no2)")
+            answer = no1 * no2
+            generateMulAnswers(answer: answer, maxResult: Operation.mul.maxAnswer(number: no1, level: level))
+        case .div:
+            question = String("\(no2*no1) / \(no1)")
+            answer = no2
+            //
+        case .add:
+            if no2 > no1 { question = String("\(no2) + \(no1)") }
+            else { question = String("\(no1) + \(no2)") }
+            answer = no1 + no2
+            //
+        case .sub:
+            if no2 > no1 {
+                question = String("\(no1) - \(no2)")
+                answer = no2 - no1
+            } else {
+                question = String("\(no1) - \(no2)")
+                answer = no1 - no2
+            }
+            //
+        }
+        answers.shuffle()
+        for k in 0..<4 {
+            if answers[k] == answer { correctAnswerIdx = k }
+        }
+        
+        print("start...")
+        for l in 0..<answers.count {
+            print("\(answers[l])")
+        }
+    }
+    
+    mutating func generateMulAnswers(answer: Int, maxResult: Int) {
+        answers.append(answer)
+        var randomAnswer: Int
+        for _ in 1...3 {
+            repeat {
+                randomAnswer = Int.random(in: 0...maxResult)
+            } while answers.contains(randomAnswer)
+            answers.append(randomAnswer)
+        }
     }
 }
 
 struct Questions {
     var questions: [Question] = []
     let noQuestions: Int
-    let maxFactor: Int
-    var answers: [Int] = []
-    var correctAnswerIdx = 0
-    let eduFactor: Int
+    let eduNumber: Int
     
-    init(eduFactor: Int, maxFactor: Int, noQuestions: Int) {
+    init(eduNumber: Int, mul: Bool, div: Bool, add: Bool, sub: Bool, level: Level, noQuestions: Int) {
         self.noQuestions = noQuestions
-        self.maxFactor = maxFactor
-        self.eduFactor = eduFactor
+        self.eduNumber = eduNumber
+        let operators = Operators(mul: mul, div: div, add: add, sub: sub, noOfQuestions: noQuestions)
         
+        if mul { generateMulQnA(eduNumber: eduNumber, level: level, noQuestions: operators.mulQuestions) }
+        if div { generateDivQnA(eduNumber: eduNumber, level: level, noQuestions: operators.divQuestions) }
+        if add { generateAddQnA(eduNumber: eduNumber, level: level, noQuestions: operators.addQuestions) }
+        if sub { generateSubQnA(eduNumber: eduNumber, level: level, noQuestions: operators.subQuestions) }
+        
+        questions.shuffle()
+    }
+
+    mutating func generateMulQnA(eduNumber: Int, level: Level, noQuestions: Int) {
+        let maxFactor = Operation.mul.maxNumber(level: level)
+        print("maxFactor: \(maxFactor)")
         if noQuestions == 0 {
             for i in 0...maxFactor {
-                questions.append(Question(factor1: eduFactor, factor2: i))
+                questions.append(Question(operation: Operation.mul, level: level, no1: eduNumber, no2: i))
             }
         } else {
             if noQuestions > maxFactor {
                 for i in 0..<noQuestions {
                     if i <= maxFactor {
-                        questions.append(Question(factor1: eduFactor, factor2: i))
+                        questions.append(Question(operation: Operation.mul, level: level, no1: eduNumber, no2: i))
                     } else {
-                        questions.append(Question(factor1: eduFactor, factor2: Int.random(in: 0...maxFactor)))
+                        questions.append(Question(operation: Operation.mul, level: level, no1: eduNumber, no2: Int.random(in: 0...maxFactor)))
                     }
                 }
             } else {
                 for i in 0...maxFactor {
-                    questions.append(Question(factor1: eduFactor, factor2: i))
+                    questions.append(Question(operation: Operation.mul, level: level, no1: eduNumber, no2: i))
                 }
                 var j = maxFactor
                 while j >= noQuestions {
@@ -131,34 +261,17 @@ struct Questions {
                 }
             }
         }
-        questions.shuffle()
-        generateAnswers(activeQuestion: 0)
     }
-    
-    mutating func generateAnswers(activeQuestion: Int) {
-        answers.removeAll()
-        answers.append(questions[activeQuestion].answer)
-        for i in 0...(maxFactor*eduFactor) {
-            if i != answers[0] {
-                answers.append(i)
-            }
-        }
-        var j = (maxFactor*eduFactor)
-        while j >= 3 {
-            answers.remove(at: Int.random(in: 1...j))
-            j -= 1
-        }
-        answers.shuffle()
-        for k in 0..<3 {
-            if answers[k] == questions[activeQuestion].answer { correctAnswerIdx = k }
-        }
-        
-        print("start...")
-        for l in 0..<answers.count {
-            print("\(answers[l])")
-        }
-        
+
+    func generateDivQnA(eduNumber: Int, level: Level, noQuestions: Int) {
     }
+
+    func generateAddQnA(eduNumber: Int, level: Level, noQuestions: Int) {
+    }
+
+    func generateSubQnA(eduNumber: Int, level: Level, noQuestions: Int) {
+    }
+
 }
 
 struct ContentView: View {
@@ -167,11 +280,12 @@ struct ContentView: View {
     @State private var score = 0
     
     var body: some View {
-        var questions = Questions(eduFactor: 8, maxFactor: 20, noQuestions: 6)
+        let questions = Questions(eduNumber: 8, mul: true, div: false, add: false, sub: false, level: .hard, noQuestions: 6)
 
         Group {
             if userInputScreen {
-                // collect startup parameters
+  //              // collect startup parameters
+                Text("Hej")
             } else {
                 ZStack {
                     LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom)
@@ -189,12 +303,12 @@ struct ContentView: View {
                             .foregroundColor(.white)
                             .clipShape(RoundedRectangle(cornerRadius: 20))
                             .onAppear {
-                                questions.generateAnswers(activeQuestion: activeQuestionNo)
+//                                questions.generateAnswers(activeQuestion: activeQuestionNo)
                             }
                         
-                        ForEach(0..<3) { i in
+                        ForEach(0..<4) { i in
                             Button(action: { print("Trykket på \(i)")}) {
-                                Text("\(questions.answers[i])")
+                                Text("\(questions.questions[activeQuestionNo].answers[i])")
                             }
                         }
                         
@@ -206,7 +320,6 @@ struct ContentView: View {
                 }
             }
         }
-
 
 /*        List {
             ForEach(0..<questions.questions.count) { i in
